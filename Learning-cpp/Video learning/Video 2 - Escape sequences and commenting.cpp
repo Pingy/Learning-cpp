9b10340a079f1d7f1a4e1b4a2a8119b59e05b846
Pingy <jojosmithey@gmail.com>
2016-04-22 18:49:08 -0400

    E to the Z so far

---
+++ Learning-cpp/Video learning/Video 2 - Escape sequences and commenting.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int escapecommenting() {
+
+	cout << "\"Hello\" \\World!\n";
+	// This is just an average comment dude
+
+	/*
+	yo
+	dude
+	*/
+	return 0;
+}
---
+++ Learning-cpp/Video learning/Video 3 - Arithmetic operators.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int arithmetic() {
+
+	cout << 5 + 8 << endl;
+	cout << "Order of operations in cpp is brackets, multiplication, division, modulus, addition, and finally subtraction." << endl;
+
+	return 0;
+}
